9e24a99346ef7a51ba3e705f5945c6d21220c4a7
sheldon <sheldonshi2008@gmail.com>
2012-07-04 18:22:39 -0700

    fix web UI after adding a workspace

--- public/javascripts/mytinytodo.js
+++ public/javascripts/mytinytodo.js
@@ -905,7 +905,7 @@ function addProjectDialog() {
 function addProject(title) {
     _mtt.db.request('addProject', {title:title}, function(json){
         if(!parseInt(json.total)) return;
-        var htmlStr='';
+        var htmlStr=_mtt.lang.get('workspaces') + '&nbsp;&nbsp;&nbsp;';
         $.each(json.list, function(i,item){
             if (i==0) _mtt.project=item.project.id;
             htmlStr += '<a href="#" id="project_'+item.project.id+'_'+item.role+'" class="weak">'+item.project.title+'</a>&nbsp;&nbsp;&nbsp;';
